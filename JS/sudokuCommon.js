438ce0ed83089ed640e452dc41c4c6e43315cc67
HubertBertling <h.bertling@web.de>
2024-07-03 21:29:36 +0200

    v28

--- JS/sudokuCommon.js
+++ JS/sudokuCommon.js
@@ -1,5 +1,5 @@
 let sudoApp;
-let VERSION = 27;
+let VERSION = 28;
 
 // ==========================================
 // Basic classes
--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ var APP_PREFIX = 'sudo_';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_27';
+var VERSION = 'version_28';
 
 // The files to make available for offline use. make sure to add 
 // others to this list
@@ -30,7 +30,6 @@ var URLS = [
   `${GHPATH}/images/download.png`,
   `${GHPATH}/images/drucker.png`,
   `${GHPATH}/images/fail.png`,
-  `${GHPATH}/images/glueckwunsch-Vorlage.jpg`,
   `${GHPATH}/images/glueckwunsch.jpg`,
   `${GHPATH}/images/import.png`,
   `${GHPATH}/images/info.png`,
